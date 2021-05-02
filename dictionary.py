db1183f11e56a3974848b63ec5d17032adf63189
Bikash Subedi <biikashsubedi@gmail.com>
2021-05-02 21:37:51 +0545

    python dictionary works with mysql

--- dictionary.py
+++ dictionary.py
@@ -1,31 +1,22 @@
-import json
+import mysql.connector
 from difflib import get_close_matches
 
-data = json.load(open('files/dictionary.json'))
+con = mysql.connector.connect(
+    user='root',
+    password='bikash',
+    host='127.0.0.1',
+    database='pythonDictionary'
+)
+cursor = con.cursor()
 
+word = input('please choose word for definition: ')
 
-def dictionary(word):
-    # w = SequenceMatcher(word.lower())
-    if word in data:
-        return data[word]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        yes_no = input(
-            "Did you mean %s instead? Enter Y for Yes and N for No: " % get_close_matches(word, data.keys())[0])
-        if yes_no == "Y":
-            return data[get_close_matches(word, data.keys())[0]]
-        elif yes_no == 'N':
-            return f'{word} does not exist☹️, Please try another'
-        else:
-            return 'Wrong Input, Please choose Y or N'
-    else:
-        return f'{word} does not found ☹️, Please try another'
+query = cursor.execute(f"SELECT * FROM Dictionary where title = '{word}' ")
+results = cursor.fetchall()
 
-
-word = input('Enter word to get definition: ')
-result = dictionary(word)
-
-if type(result) == list:
-    for item in result:
-        print(item)
+print('========================================================================')
+if results:
+    for item in results:
+        print(item[2])
 else:
-    print(result)
+    print('word does not found ☹️, Please try another')
